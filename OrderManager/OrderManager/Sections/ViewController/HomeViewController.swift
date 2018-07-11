d4880c01af6dbc3fe47bd4a4c77d4d38e1beea69
ZCC <18311296151@163.com>
2018-07-11 13:47:35 +0800

    初始化配置

--- OrderManager/OrderManager/Sections/ViewController/HomeViewController.swift
+++ OrderManager/OrderManager/Sections/ViewController/HomeViewController.swift
@@ -12,24 +12,46 @@ class HomeViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.addSubview(self.tableView)
     }
 
+    lazy var tableView: UITableView = { [unowned self] in
+        let tableView = UITableView.init(frame: CGRect.init(x: 0.0, y: 0.0, width: kWidth, height: kHeight))
+        tableView.tableFooterView = UIView()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+
+    lazy var viewModel: HomeViewModel = {
+        let viewModel = HomeViewModel()
+        
+        return viewModel
+    }()
+    
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 0
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
 }
+
